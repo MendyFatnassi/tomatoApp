5eed6d0fb8862be1746e24dd59057a4bc98371e7
Mendy Fatnassi <fatmendy72@hotmail.fr>
2023-10-04 22:10:13 +0200

    Init commit

---
+++ src/main/java/com/example/tomotaapp/TomotaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.tomotaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomotaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomotaAppApplication.class, args);
+    }
+
+}
